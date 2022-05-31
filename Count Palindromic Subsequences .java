6e233ec2be9e03022ce26eaac00f2867e54961fe
Divyashree Agrawal <64089485+Divyashree356@users.noreply.github.com>
2022-05-31 20:57:37 +0530

    Create Count Palindromic Subsequences .java

---
+++ Count Palindromic Subsequences .java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+import java.util.*;
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String str = sc.nextLine();
+			//System.out.println(str.length());
+			Solution ob  = new Solution();
+			System.out.println(ob.countPS(str));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+/*You are required to complete below method */
+
+class Solution
+{
+    long countPS(String str)
+    {
+        long mod= 1000000007;
+        int n=str.length();
+        long[][] dp=new long[n][n];
+        
+        for(int gap=0;gap<n;gap++)
+        {
+            for(int i=0,j=gap;j<n;i++, j++)
+            {
+                if(gap==0)
+                 dp[i][j]=1;
+                else if(gap==1)
+                 dp[i][j]= (str.charAt(i)==str.charAt(j)?3:2) ;
+                else
+                {
+                    if(str.charAt(i)==str.charAt(j))
+                      dp[i][j]= (dp[i][j-1]%mod + dp[i+1][j]%mod +1) % mod;
+                    else
+                     dp[i][j]= (dp[i][j-1]%mod + dp[i+1][j]%mod - dp[i+1][j-1]%mod +mod)%mod;
+                }
+            }
+        }
+        return dp[0][n-1]%mod;
+    }
+}
